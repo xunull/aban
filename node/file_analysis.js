1de389d69bb03479ec126f2a92dc1df7d78fd46b
崔启坤 <todogo@vip.163.com>
2016-07-25 22:43:49 +0800

    添加异步统计文件数量方法

--- node/file_analysis.js
+++ node/file_analysis.js
@@ -56,7 +56,8 @@ function analysisSync(_path, tree = _tree) {
 let fileSum = 0;
 let dirSum = 0;
 
-function countNumSync(_path,rank=0) {
+function countNumSync(_path, rank = 0) {
+
     var files = fs.readdirSync(_path);
     for (var file of files) {
         var stats = fs.statSync(path.join(_path, file));
@@ -69,42 +70,78 @@ function countNumSync(_path,rank=0) {
                 // 属于排除的,不扫描的目录
             } else {
                 dirSum++;
-                countNumSync(path.join(_path, file),rank+1);
+                countNumSync(path.join(_path, file), rank + 1);
             }
         }
     }
 
     if (rank === 0) {
-        console.log('文件的数量为 ',fileSum);
-        console.log('文件夹的数量为 ',dirSum);
+        console.log('文件的数量为 ', fileSum);
+        console.log('文件夹的数量为 ', dirSum);
     }
+
 }
 
 // 统计文件夹,文件的数量
 // 还是同样的问题,什么时候知道遍历完了 , 同步的方法会知道什么时候调用完了
-function countNum() {
+// for 循环中的异步有顺序么？如果没有顺序完全是任意的，那么怎么知道都遍历完了
+function countNum(_path, parentWaiter = null, rank = 0, maxRank = 0) {
+    // console.log('开始执行');
     fs.readdir(_path, function(err, files) {
+        var waiter = new Waiter();
         if (err) {
-            console.log(err);
-            console.log('读取文件夹 ', path, ' 出错');
+            // console.log(err);
+            console.log('读取文件夹 ', _path, ' 出错');
         } else {
-            for (var file of files) {
+            waiter.waitingFor(files.length, function() {
+                if (parentWaiter === null) {
+                    console.log('所有的都已经遍历完毕');
+                    console.log('文件的数量为 ', fileSum);
+                    console.log('文件夹的数量为 ', dirSum);
+                } else {
+
+                    parentWaiter.someDone();
+                }
+
+            });
+
+            // 这个地方,如果是使用的异步操作，是不能使用var 声明的,
+            // 如果使用var 声明，file 最终的值会是最后一次循环的值,并且这个值是在整个for中可见的,
+            // 因此需要使用let声明
+            for (let file of files) {
+                // console.log(file);
                 fs.stat(path.join(_path, file), function(err, stats) {
                     if (stats.isFile()) {
+                        // console.log(path.join(_path, file) ,' 是文件');
                         fileSum++;
+                        waiter.someDone();
+
                     } else if (stats.isDirectory()) {
+                        // console.log(path.join(_path, file) ,' 是文件夹');
                         if (config.excludeDirs.includes(file)) {
                             // 属于排除的,不扫描的目录
+                            // console.log(path.join(_path, file) ,' 是排除的文件夹');
+
+                            waiter.someDone();
+
                         } else if (file.startsWith('.')) {
                             // 属于排除的,不扫描的目录
+                            // console.log(path.join(_path, file) ,' 是排除的文件夹');
+
+                            waiter.someDone();
+
                         } else {
                             dirSum++;
-                            analysis(path.join(_path, file), tree.get(file));
+                            if (rank > maxRank) {
+                                maxRank++;
+                            }
+                            // console.log(path.join(_path, file) ,' 继续遍历');
+                            countNum(path.join(_path, file), waiter, rank + 1, maxRank);
                         }
                     }
                 });
             }
-            console.log(files);
+
         }
     });
 }
@@ -193,3 +230,4 @@ function analysis_extension(filename) {
 exports.analysis = analysis;
 exports.analysisSync = analysisSync;
 exports.countNumSync = countNumSync;
+exports.countNum = countNum;
