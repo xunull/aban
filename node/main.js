e44f2b22172f8c39d8bf1c29eeec3b87bdce8058
崔启坤 <todogo@vip.163.com>
2016-08-01 22:58:50 +0800

    add some test func

--- node/main.js
+++ node/main.js
@@ -1,11 +1,35 @@
 var exec= require('child_process').exec;
 var file_analysis=require('./file_analysis');
 
-exec('echo $my_github_workspaces',function(err,stdout,stderr){
-  if(err) {
-    console.log('获取github工作路径出错');
-  } else {
-    file_analysis.analysisSync(stdout.replace('\n',''));
-  }
+// exec('echo $my_github_workspaces',function(err,stdout,stderr){
+//   if(err) {
+//     console.log('获取github工作路径出错');
+//   } else {
+//     file_analysis.analysisSync(stdout.replace('\n',''));
+//   }
+//
+// });
 
-});
+
+// exec('echo $my_github_workspaces',function(err,stdout,stderr){
+//   if(err) {
+//     console.log('获取github工作路径出错');
+//   } else {
+//     file_analysis.countNumSync(stdout.replace('\n',''));
+//   }
+//
+// });
+
+// exec('echo $my_github_workspaces',function(err,stdout,stderr){
+//   if(err) {
+//     console.log('获取github工作路径出错');
+//   } else {
+//     file_analysis.countNum(stdout.replace('\n',''));
+//   }
+//
+// });
+
+
+// file_analysis.countNumSync('/Users/cuiqikun/xunull/Github/xunull/yuwang');
+
+file_analysis.countNum('/Users/cuiqikun/xunull/Github/xunull/yuwang');
